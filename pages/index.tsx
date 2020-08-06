b2b51899fa4f0a00c8114da71f300164b7bf762b
anluordige <anluoridge@gmail.com>
2020-08-06 11:55:24 +1000

    Add TypeScript support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import { Text, Button, View } from 'react-native'
+import React from 'react';
+import { Text, Button, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { Link } from 'expo-next-react-navigation'
 
 export default function App() {
+  const me: number = 1;
   return (
       <NavigationContainer>
         <View>
           <Text>Welcome to Index (landing) Page</Text>
-          
+          <Button onPress={() => console.table(['one', 'two'])} title="Click me" />
           <Link routeName="app" params={{ initialRouteName: 'Other', key: 'blah'}} web={{ as: '.'}}>
-            <Button 
+            <Button
             style={{ userSelect:'none' }}
             onPress={() => {console.log("pressed")}}
             title="Go to App"></Button>
